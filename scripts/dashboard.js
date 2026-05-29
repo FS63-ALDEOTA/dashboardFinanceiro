efa4b5e9f0fe2054c42da70cff04bac870933358
nayaracalenzo <queiroz.14@hotmail.com>
2026-05-28 21:43:05 -0300

    métodos de arrays

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,11 +1,31 @@
-let addTransacao = []
+let transacoes = []
+const dados = document.getElementById("dados")
+const cardEntradas = document.getElementById("entradas")
+const cardSaidas = document.getElementById("saidas")
+const cardSaldo = document.getElementById("saldo")
+
+function renderizarTabela () {
+  if (transacoes.length === 0) {
+    dados.innerHTML = `
+    <tr>
+      <td colspan='4' class='text-center py-4 px-6 text-lg border-b'>
+        Nenhuma transação cadastrada
+      </td>
+    </tr>
+    `
+  }
+  calcularTotais()
+}
+
+renderizarTabela()
+
 
 function adicionarTransacao (event) {
   event.preventDefault()
   const desc = document.getElementById("desc").value
   const valor = parseFloat(document.getElementById("valor").value)
   const tipo = document.getElementById("tipo").value
-  const dados = document.getElementById("dados")
+
 
   const transacao = {
     data: new Date().toLocaleDateString("pt-br"),
@@ -16,18 +36,39 @@ function adicionarTransacao (event) {
 
   console.log(transacao)
 
-  addTransacao.push(transacao)
+  transacoes.push(transacao)
 
-  dados.innerHTML = `${ addTransacao.map((item, index)=>  (
+  dados.innerHTML = `${ transacoes.map((item, index)=>  (
       `<tr key=${index}>
-        <td>${item.data}</td>
-        <td>${item.desc}</td>
-        <td>${item.tipo}</td>
-        <td>${item.valor}</td>
+        <td class="px-6 py-4 whitespace-nowrap">${item.data}</td>
+        <td class="px-6 py-4 whitespace-nowrap">${item.desc}</td>
+        <td class=${item.tipo === "entrada" ? "text-blue-500 font-bold" :"text-red-500 font-bold"} px-6 py-4 whitespace-nowrap>${item.tipo}</td>
+        <td class="px-6 py-4 whitespace-nowrap">${item.valor}</td>
       </tr>`
     )
-  )}
+  ).join("")}
   `
+
+  renderizarTabela()
 }
 
-console.log(addTransacao)
+function calcularTotais () {
+  const entradas = transacoes.filter((transacao)=> (
+      transacao.tipo === "entrada"
+  ))
+  .reduce((acc, transacao)=> (acc + transacao.valor), 0)
+  
+  const saidas = transacoes.filter((transacao)=> (
+      transacao.tipo === "saida"
+  )).reduce((acc, transacao) => (acc + transacao.valor), 0)
+
+  const saldo = entradas - saidas
+  console.log(saldo)
+
+  cardSaldo.textContent = `R$ ${saldo.toFixed(2)}`
+  cardEntradas.textContent = `R$ ${entradas.toFixed(2)}`
+  cardSaidas.textContent = `R$ ${saidas.toFixed(2)}`
+
+  console.log("entradas:",entradas)
+  console.log("saidas:",saidas)
+}
